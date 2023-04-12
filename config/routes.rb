51e88919a69fa2e1bb5d981cc3cafaadad1fe679
enoqueJonas <enoque.macanda@gmail.com>
2023-04-12 15:08:27 +0200

    ADD authentication with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :group_entities
-  resources :entities
-  resources :groups
-  resources :users
+  devise_for :users
+  resources :users do 
+    resources :group_entities
+    resources :groups do
+      resources :entities
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to: 'users#index'
 end
