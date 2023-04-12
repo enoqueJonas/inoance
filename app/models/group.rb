2041bbfdef55dff05dc8f196a3e0a5ce8abb14b2
enoqueJonas <enoque.macanda@gmail.com>
2023-04-12 16:46:34 +0200

    ADD functionality do add new groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :user
-  validates :name, :icon, presence: true, length: { in: 1...500 }
+  has_one_attached :avatar
+  validates :name, presence: true, length: { in: 1...100 }
 end
