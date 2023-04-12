9667248ae67398d56ac989b8d4af8a782e64048b
enoqueJonas <enoque.macanda@gmail.com>
2023-04-12 15:20:43 +0200

    ADD validations for Entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,5 @@
 class Entity < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { in: 1...100 }
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
